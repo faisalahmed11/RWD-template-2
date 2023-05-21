b5ccb5de9c4011b9da5cc4a7f401fb37cdc26cf8
Faisal Ahmed <92742016+faisalahmed11@users.noreply.github.com>
2023-05-21 15:33:27 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,8 @@ import Head from 'next/head'
 export default function App({ Component, pageProps }) {
   return <>
 <Head>
-        <title>RWD-template-2</title>
-        <meta name="description" content="This is a responsive web design template" />
+        <title>Responsive-Architect-Site</title>
+        <meta name="description" content="This is a responsive website about Architect" />
         
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
